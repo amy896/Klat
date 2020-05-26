df8b614ae4054af3b687fc16f3dcfca7c3f6e4b0
amy896 <53885635+amy896@users.noreply.github.com>
2020-05-26 15:15:34 +0900

    member dao/mappe 회원 가입 기능 추가

--- src/main/java/com/fanta/klat/dao/MemberDao.java
+++ src/main/java/com/fanta/klat/dao/MemberDao.java
@@ -1,5 +1,17 @@
 package com.fanta.klat.dao;
 
+import com.fanta.klat.model.Member;
+
 public interface MemberDao {
+
+	public int insertMember(Member member);
+	
+	public int insertAuthority(int mNum);
+	
+	/*
+	public Member selectMemberByMId(String mId);
+	
+	public Member selectMemberByMNum(int mNum);
+	*/
 
 }
