c3e63310accba5a483d9de4e5c83b639febd9760
amy896 <53885635+amy896@users.noreply.github.com>
2020-05-26 15:15:55 +0900

    회원 가입 기능 추가

--- src/main/java/com/fanta/klat/controller/MemberController.java
+++ src/main/java/com/fanta/klat/controller/MemberController.java
@@ -1,5 +1,28 @@
 package com.fanta.klat.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.fanta.klat.model.Member;
+import com.fanta.klat.service.MemberService;
+
+@Controller
+@RequestMapping("/member")
 public class MemberController {
 
+	@Autowired
+	MemberService memberService;
+	
+	@RequestMapping(value = "/signUpForm", method = RequestMethod.GET)
+	public String showSignUpForm() {
+		return "member/signUpForm";
+	}
+	
+	@RequestMapping(value = "/signUpMember", method = RequestMethod.POST)
+	public String signUpMember(Member member) {
+		memberService.signUpMember(member);
+		return null;
+	}
 }
