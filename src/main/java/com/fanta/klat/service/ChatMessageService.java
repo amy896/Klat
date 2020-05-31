965df0e7dd5a6e59666bd85ac2bf681910596097
hyeseon-park <lightning350@naver.com>
2020-05-31 18:10:22 +0900

    채팅메시지 보내기 & 불러오기 기능 추가

---
+++ src/main/java/com/fanta/klat/service/ChatMessageService.java
@@ -0,0 +1,26 @@
+package com.fanta.klat.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fanta.klat.dao.ChatMessageDao;
+import com.fanta.klat.model.ChatMessage;
+
+@Service
+public class ChatMessageService {
+	@Autowired
+	private ChatMessageDao cmDao;
+
+	public boolean sendChatMessage(ChatMessage chatMessage) {
+		if (cmDao.insertChatMessage(chatMessage) > 0) {
+			return true;
+		}
+		return false;
+	}
+	
+	public List<ChatMessage> getAllChatMessageByCrNum(int crNum) {
+		return cmDao.selectAllChatMessageByCrNum(crNum);
+	}
+}
